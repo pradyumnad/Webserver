3392c645b34445069d7f4fdb61c7b8bc6b8bb16b
pradyumnad <pradyumnadoddala@gmail.com>
2015-04-12 20:44:21 -0500

    Included 404 page and index page.

--- myserver/server.py
+++ myserver/server.py
@@ -4,10 +4,9 @@ __author__ = 'pradyumnad'
 
 from socket import *
 import re
+import os
 
 PORT = 12345
-
-
 
 def getrequestdata(data):
     string = bytes.decode(data)
@@ -24,6 +23,7 @@ if __name__ == '__main__':
     serverSocket = socket(AF_INET, SOCK_STREAM)
     host = gethostname()
     info = getaddrinfo(host, PORT)
+    print(info)
     serverSocket.bind((host, PORT))
     serverSocket.listen(5)
 
@@ -42,14 +42,24 @@ if __name__ == '__main__':
 
             filename = reqdata["FileName"]
             print("Opening "+filename)
-            f = open(filename)
 
-            client.send(f.read())
-            # client.send("Thank you for connecting")
-            print("HTML sent to Client.")
+            if len(filename) == 0:
+                filename = "HelloWorld.html"
+
+            if os.path.isfile(filename):
+                f = open(filename)
+                print "file does exist at this time"
+                client.send(f.read())
+                print("HTML sent to Client.")
+            else:
+                filename = "404Error.html"
+                print("Opening "+filename)
+                f = open(filename)
+                client.send(f.read())
+
         except IOError:
             print("Exception.")
             client.send("HTTP/1.1 404, File not found.\n")
 
         client.close()
-    serverSocket.close()
+    # serverSocket.close()
